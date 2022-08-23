93ff631f26e537a578c18bbdb230dd688e6ab99e
GiYeong Um <eomky2005@gmail.com>
2022-08-23 22:47:09 +0900

    [fix] util 설명문구 추가

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,20 +1,26 @@
 import 'package:intl/intl.dart';
 
+  /// 이메일 정규표현식
   checkEmailRegex({required String email}) {
     return RegExp(r"^[a-zA-Z\d.a-zA-Z\d.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z\d]+\.[a-zA-Z]+").hasMatch(email);
   }
+
+  /// 닉네임 정규표현식 한글, 영문, 숫자, 공백 최소 최대치
   checkNickNameRegex({required String nickName, required int min, required int max}) {
     return RegExp("^[ㄱ-ㅎ가-힣0-9a-zA-Z ]{$min,$max}\$").hasMatch(nickName.trim());
   }
 
+  /// SMS 인증코드 정규표현식
   checkSMSCodeRegex({required String code, required int length}) {
     return RegExp("^[0-9]{$length}\$").hasMatch(code);
   }
 
+  /// 비밀번호 정규표현식 대,소문자 영문, 숫자, 특수문자 포함 n~n 자
   checkPasswordRegex({required String password, required int min, required int max}) {
     return RegExp('^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@!%*#?~^,.&+=])[A-Za-z0-9@!%*#?~^,.&+=]{$min,$max}\$').hasMatch(password);
   }
 
+  /// n시간전 등의 기능
   timeAgo({required DateTime dateTime,bool numericDates = true}) {
     final dateNow = DateTime.now();
     final difference = dateNow.difference(dateTime);
@@ -37,6 +43,7 @@ import 'package:intl/intl.dart';
     }
   }
 
+  /// 1000 이상의 수 를 n K 로 바꿔주는 함수
   thousandToK (int value) {
     if(value > 999){
       double temp = value / 1000;
